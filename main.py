e8441d3f60d3821abd9993990b31d12269e4072a
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2026-04-20 12:34:57 -0400

    Start of setup for main.py

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from routes import runs
+from fastapi import FastAPI
+from database import Runs
+from contextlib import asynccontextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+DATABASE_URL = "sqlite+pysqlite:///:memory:"
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Create the engine
+    engine = create_engine(DATABASE_URL, echo=True)
+    # Create the session
+    session = Session(engine)
+    # Create all the tables and check to see if they exist.
+    Runs.metadata.create_all()
+    yield
+    pass
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 def main():
